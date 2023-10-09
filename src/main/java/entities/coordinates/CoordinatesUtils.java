ef559a88692c60e17490590de9e8b52af3fac904
Azus <a.ben.bouhajja@gmail.com>
2023-10-09 09:53:02 +0200

    write the MapCoordinatesTest methods

---
+++ src/main/java/entities/coordinates/CoordinatesUtils.java
@@ -0,0 +1,12 @@
+package entities.coordinates;
+
+public abstract class CoordinatesUtils {
+    public static boolean isCoordinatesInMapLimits(Coordinates mapLimits, Coordinates adventurerNextCoordinates) {
+
+        /**
+         * TODO
+         * write the is coordinates in map limits method
+         */
+        return true;
+    }
+}
--- src/test/java/entities/coordinates/MapCoordinatesTest.java
+++ src/test/java/entities/coordinates/MapCoordinatesTest.java
@@ -2,15 +2,24 @@ package entities.coordinates;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class MapCoordinatesTest {
 
     @Test
     public void should_return_true_if_coordinates_in_map_limits() {
+        Coordinates mapLimits = new Coordinates(1, 1);
+        Coordinates coordinates = new Coordinates(0, 0);
 
+        assertTrue(CoordinatesUtils.isCoordinatesInMapLimits(mapLimits, coordinates));
     }
 
     @Test
     public void should_return_false_if_coordinates_not_in_map_limits() {
+        Coordinates mapLimits = new Coordinates(1, 1);
+        Coordinates coordinates = new Coordinates(1, 0);
 
+        assertFalse(CoordinatesUtils.isCoordinatesInMapLimits(mapLimits, coordinates));
     }
 }
