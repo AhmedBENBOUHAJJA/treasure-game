89e98d49aacc6b653dc0e3695f4390b92ff5e0b2
Azus <a.ben.bouhajja@gmail.com>
2023-10-09 09:42:27 +0200

    implementing the FileReader class file to run the test with success

--- src/main/java/entities/file/FileReader.java
+++ src/main/java/entities/file/FileReader.java
@@ -1,17 +1,27 @@
 package entities.file;
 
+import entities.exceptions.FileNotFoundException;
 import entities.exceptions.ProgramException;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public  abstract class FileReader {
 
     public static List<String> getFileLines(String filePath) throws ProgramException {
 
-        /**
-         * TODO
-         * write the get file lines method to run the test with success
-         */
-        return null;
+        List<String> fileLine = new ArrayList<>();
+
+        try (Stream<String> stream = Files.lines(Path.of(filePath))) {
+            stream.forEach(fileLine::add);
+        } catch (IOException ioe) {
+            throw new FileNotFoundException("you can not read the file!");
+        }
+
+        return fileLine;
     }
 }
